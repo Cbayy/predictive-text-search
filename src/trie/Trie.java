a6defa1fd19eec19aadf1d958c2d9b247a22cf49
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-11-25 18:41:31 +1030

    Added insert into trie method.

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -1,4 +1,26 @@
 package trie;
 
 public class Trie {
+
+    private TrieNode root = new TrieNode();
+
+    public TrieNode insert(String str, TrieData data){
+        TrieNode current = root;
+
+        for(int i = 0; i < str.length(); i++){
+
+            char childChar = str.charAt(i);
+            TrieNode child = current.getChild(childChar);
+
+            if(child == null){
+                child = new TrieNode();
+                current.addChild(str.charAt(i), child);
+            }
+            current = child;
+        }
+        current.addData(data);
+        current.setTerminal(true);
+
+        return current;
+    }
 }
