88cab040b191ecffcc1ce80999195929ddadae6a
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-11-26 11:32:53 +1030

    Added method to return most frequent suffix from trie.

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -1,7 +1,10 @@
 package trie;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Trie {
 
@@ -53,10 +56,53 @@ public class Trie {
     }
 
     public String getMostFrequentWordWithPrefix(String prefix){
+        TrieNode start = getNode(prefix);
+
+        if(start == null){
+            return null;
+        }
 
         String suffix = "";
+        LinkedList<TrieNode> allChildren = new LinkedList<>();
+        int mostFreq = 0;
+        Character next = null;
 
-        
+        //Loops through all the children in the current node (start) and finds the highest frequency child
+        for(Map.Entry<Character, TrieNode> child: start.children.entrySet()){
+            allChildren = findAll(child.getValue(), prefix, allChildren);
+            TrieNode startNode;
+            for(TrieNode t: allChildren){
+                if(t.getData().getFrequency() > mostFreq) {
+                    mostFreq = t.getData().getFrequency();
+                    startNode = t;
+                    next = child.getKey();
+                }
+            }
+
+        }
+
+        //Appends the result to the empty/current string, recursively calls itself
+        suffix = String.valueOf(next);
+        suffix = getMostFrequentWordWithPrefix(prefix + suffix);
+
+        //If the found suffix has a lower frequency than the current prefix, returns prefix
+        if(start.isTerminal() && start.getData().getFrequency() > mostFreq){
+            return prefix;
+        }
         return suffix;
     }
+
+
+    public LinkedList<TrieNode> findAll(TrieNode curr, String prefix, LinkedList<TrieNode> allChildren){
+        for(Map.Entry<Character, TrieNode> set: curr.children.entrySet()){
+            findAll(set.getValue(), prefix.concat(String.valueOf(set.getKey())), allChildren);
+
+        }
+        if(curr.isTerminal()){
+            allChildren.add(curr);
+        }
+        return allChildren;
+
+    }
+
 }
