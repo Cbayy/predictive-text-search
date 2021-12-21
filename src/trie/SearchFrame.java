df691755009e33907ce624e3f8df7e49db76e659
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-12-21 16:33:33 +1030

    Added commit action for frame and runnable to main.

--- src/trie/SearchFrame.java
+++ src/trie/SearchFrame.java
@@ -3,16 +3,21 @@ package trie;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.event.ActionEvent;
 
 public class SearchFrame extends JFrame implements DocumentListener {
 
     private JTextArea textArea;
+    private String fileName = "data/word-freq.expanded.trim.txt";
+    private Trie dictionaryTrie = null;
 
     private static final String COMMIT_ACTION = "commit";
 
     private static enum Mode {
         INSERT, COMPLETION
     };
+
+    private Mode mode = Mode.INSERT;
 
     public SearchFrame(){
         super("Predictive Text");
@@ -22,6 +27,9 @@ public class SearchFrame extends JFrame implements DocumentListener {
         InputMap im = textArea.getInputMap();
         ActionMap am = textArea.getActionMap();
         im.put(KeyStroke.getKeyStroke("ENTER"), COMMIT_ACTION);
+        am.put(COMMIT_ACTION, new CommitAction());
+
+        dictionaryTrie = Trie.readInDictionary(fileName);
     }
 
     @Override
@@ -39,5 +47,26 @@ public class SearchFrame extends JFrame implements DocumentListener {
 
     }
 
+    private class CommitAction extends AbstractAction{
+        public void actionPerformed(ActionEvent ev) {
+            if (mode == Mode.COMPLETION) {
+                int pos = textArea.getSelectionEnd();
+                textArea.insert(" ", pos);
+                textArea.setCaretPosition(pos + 1);
+                mode = Mode.INSERT;
+            } else {
+                textArea.replaceSelection("\n");
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SearchFrame().setVisible(true);
+            }
+        });
+    }
 
 }
