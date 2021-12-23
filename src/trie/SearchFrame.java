0b7079bf03e767101953a8c65014041f0171985e
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-12-23 21:36:10 +1030

    Added frame layout specs and task completion runnable.

--- src/trie/SearchFrame.java
+++ src/trie/SearchFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Locale;
 
@@ -12,6 +13,7 @@ public class SearchFrame extends JFrame implements DocumentListener {
     private JTextArea textArea;
     private String fileName = "data/word-freq.expanded.trim.txt";
     private Trie dictionaryTrie = null;
+    private JScrollPane jScrollPane1;
 
     private static final String COMMIT_ACTION = "commit";
 
@@ -23,6 +25,7 @@ public class SearchFrame extends JFrame implements DocumentListener {
 
     public SearchFrame(){
         super("Predictive Text");
+        initComponents();
 
         textArea.getDocument().addDocumentListener(this);
 
@@ -56,6 +59,16 @@ public class SearchFrame extends JFrame implements DocumentListener {
         }
         String prefix = content.substring(w+1).toLowerCase();
         String suffix = dictionaryTrie.getMostFrequentWordWithPrefix(prefix);
+
+        if(suffix != null){
+            if (!suffix.equals(prefix) && suffix.startsWith(prefix)) {
+                String completion = suffix.substring(pos-w);
+                SwingUtilities.invokeLater(new CompletionTask(completion, pos+1));
+            }
+        }else{
+            mode = Mode.INSERT;
+        }
+
     }
 
     @Override
@@ -66,6 +79,24 @@ public class SearchFrame extends JFrame implements DocumentListener {
     @Override
     public void changedUpdate(DocumentEvent e) {
 
+    }
+
+    private class CompletionTask implements Runnable{
+        String completion;
+        int position;
+
+        CompletionTask(String completion, int position) {
+            this.completion = completion;
+            this.position = position;
+        }
+
+        @Override
+        public void run() {
+            textArea.insert(completion, position);
+            textArea.setCaretPosition(position + completion.length());
+            textArea.moveCaretPosition(position);
+            mode = Mode.COMPLETION;
+        }
     }
 
     private class CommitAction extends AbstractAction{
@@ -79,6 +110,57 @@ public class SearchFrame extends JFrame implements DocumentListener {
                 textArea.replaceSelection("\n");
             }
         }
+    }
+
+    private void initComponents() {
+        textArea = new JTextArea();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        textArea.setColumns(20);
+        textArea.setLineWrap(true);
+        textArea.setRows(5);
+        textArea.setWrapStyleWord(true);
+
+        jScrollPane1 = new JScrollPane(textArea);
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+
+        //Create a parallel group for the horizontal axis
+        GroupLayout.ParallelGroup hGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+        //Create a sequential and a parallel groups
+        GroupLayout.SequentialGroup h1 = layout.createSequentialGroup();
+        GroupLayout.ParallelGroup h2 = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+        //Add a scroll panel and a label to the parallel group h2
+        h2.addComponent(jScrollPane1, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE);
+        //h2.addComponent(jLabel1, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE);
+
+        //Add a container gap to the sequential group h1
+        h1.addContainerGap();
+        // Add the group h2 to the group h1
+        h1.addGroup(h2);
+        h1.addContainerGap();
+        //Add the group h1 to hGroup
+        hGroup.addGroup(GroupLayout.Alignment.TRAILING, h1);
+        //Create the horizontal group
+        layout.setHorizontalGroup(hGroup);
+
+        //Create a parallel group for the vertical axis
+        GroupLayout.ParallelGroup vGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+        //Create a sequential group
+        GroupLayout.SequentialGroup v1 = layout.createSequentialGroup();
+        //Add a container gap to the sequential group v1
+        v1.addContainerGap();
+        //Add a label to the sequential group v1
+        //v1.addComponent(jLabel1);
+        v1.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
+        //Add scroll panel to the sequential group v1
+        v1.addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE);
+        v1.addContainerGap();
+        //Add the group v1 to vGroup
+        vGroup.addGroup(v1);
+        //Create the vertical group
+        layout.setVerticalGroup(vGroup);
+        pack();
     }
 
     public static void main(String args[]){
